5634f266a32d243446ce55041837a17751ab8006
Diptiranjanrout2000 <droutbdk999@gmail.com>
2024-08-10 11:37:31 +0530

    start

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 cd /home/pi/Node
 
 # Activate the virtual environment
-source venv/bin/activate
+source /home/pi/Node/venv/bin/activate
 
 # Run the Django development server
 python manage.py runserver 0.0.0.0:8000
