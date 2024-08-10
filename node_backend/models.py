408462284f332e688c2424caca51c3faeb488c4b
Diptiranjanrout2000 <droutbdk999@gmail.com>
2024-08-10 12:08:37 +0530

    model

--- node_backend/models.py
+++ node_backend/models.py
@@ -1,10 +1,11 @@
 from email.policy import default
 from enum import unique
+from venv import create
 # from statistics import mode
 from django.db import models
 # from . models import *
 from django.contrib.auth.models import User
-
+from datetime import datetime
 
 class Node(models.Model):
     nodeid = models.CharField(max_length=150,unique=True)
@@ -17,6 +18,7 @@ class NodeModel(models.Model):
     node_id = models.ForeignKey(Node, on_delete=models.CASCADE)
     gateway_id = models.CharField(max_length=200, blank=True)
     data_field = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
          gateway = f"{self.gateway_id}"
