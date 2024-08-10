a0a291adc6802feac536f42bd59b6953b43875d0
Diptiranjanrout2000 <droutbdk999@gmail.com>
2024-08-10 11:35:19 +0530

    star sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the Django project directory
+cd /home/pi/Node
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the Django development server
+python manage.py runserver 0.0.0.0:8000
