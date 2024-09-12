37d50807f069f2c9814aa72e79ca4405c5c2c50b
satyajitsjs <satyajitsjs3@gmail.com>
2024-09-12 17:34:09 +0530

    message

--- nodedatabase/settings.py
+++ nodedatabase/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'nodetest',
         'USER': 'bariflolabs',
         'PASSWORD': 'dipti@123',
-        'HOST': '192.168.0.142',
+        'HOST': '192.168.1.24',
         'PORT': '5432',
 
 
